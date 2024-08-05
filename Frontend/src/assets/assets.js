e8b6859fb05fe76e2fa14c68ad64da5614977d6a
Rahul  Bandgar <129932666+Rabul3755@users.noreply.github.com>
2024-08-05 11:53:50 +0530

    Update assets.js

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -60,6 +60,9 @@ import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
 
+export const url ='https://food-del-backend-ihb4.onrender.com'
+export const currency = '₹'
+
 export const assets = {
     logo,
     basket_icon,
