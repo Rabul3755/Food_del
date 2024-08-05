5a8940d04791e97a5286a470ebbdc53a5a957918
Rahul  Bandgar <129932666+Rabul3755@users.noreply.github.com>
2024-08-05 11:58:31 +0530

    Update StoreContext.jsx

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -4,8 +4,9 @@ import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const [cartItems, setCartItems] = useState({});
   const url = "https://food-del-backend-ihb4.onrender.com";
+  const [cartItems, setCartItems] = useState({});
+  const currency =''
   const [token, setToken] = useState();
   const [food_list, setFood_list] = useState([]);
 
