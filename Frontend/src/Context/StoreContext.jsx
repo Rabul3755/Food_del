ee715c7558760f2db95f48481e307297f975ccb9
Rahul  Bandgar <129932666+Rabul3755@users.noreply.github.com>
2025-10-03 15:37:35 +0530

    Update StoreContext.jsx

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const url = "http://localhost:4000";
+  const url = "https://food-del-backend-1m6q.onrender.com";
   const [cartItems, setCartItems] = useState({});
   const currency ='₹'
   const [token, setToken] = useState("");
